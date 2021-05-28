d8b799fc9be2b972ac733163f351a9d6e49c6a73
Salvador <salvadorperezchi@gmail.com>
2021-05-28 17:20:13 -0500

    AASasasa

--- VIDEOMAXX/Program.cs
+++ VIDEOMAXX/Program.cs
@@ -3,13 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using static System.Console;
 namespace VIDEOMAXX
 {
     class Program
     {
         static void Main(string[] args)
         {
+            WriteLine("\t\t\t\t\t-----------------------------------");
+            WriteLine("\t\t\t\t\t[             VIDEOMAX            ]");
+            WriteLine("\t\t\t\t\t-----------------------------------");
+            WriteLine("\n\t\t\t\t\tOpciones: ");
+            WriteLine("\t\t\t\t\t1. Buscar peliculas");
+            WriteLine("\t\t\t\t\t2. Inventario");
+            WriteLine("\t\t\t\t\t3. Ventas");
+            WriteLine("\t\t\t\t\t0. Salir");
+            WriteLine("DAVID");
         }
+    }
     }
 }

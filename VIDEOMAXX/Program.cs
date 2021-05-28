720631d6fabcda33d8b3434d5e206db1cb751c0e
Salvador <salvadorperezchi@gmail.com>
2021-05-28 17:38:25 -0500

    cubas

--- VIDEOMAXX/Program.cs
+++ VIDEOMAXX/Program.cs
@@ -19,6 +19,15 @@ namespace VIDEOMAXX
             WriteLine("\t\t\t\t\t3. Ventas");
             WriteLine("\t\t\t\t\t0. Salir");
             WriteLine("DAVID");
+            WriteLine("\t\t\t\t\t-----------------------------------");
+            WriteLine("\t\t\t\t\t[             VIDEOMAX            ]");
+            WriteLine("\t\t\t\t\t-----------------------------------");
+            WriteLine("\n\t\t\t\t\tOpciones: ");
+            WriteLine("\t\t\t\t\t1. Buscar peliculas");
+            WriteLine("\t\t\t\t\t2. Inventario");
+            WriteLine("\t\t\t\t\t3. Ventas");
+            WriteLine("\t\t\t\t\t0. Salir");
+            WriteLine("DAVID");
         }
     }
     }

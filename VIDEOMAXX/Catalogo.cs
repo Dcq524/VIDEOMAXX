5d3df82dfddba6d27f99ad8c5174f616514d9511
JOSUE ABIMAEL CAUICH CHE <A19214940@alumnos.uady.mx>
2021-05-30 17:14:25 -0500

    Busqueda de Películas

--- VIDEOMAXX/Catalogo.cs
+++ VIDEOMAXX/Catalogo.cs
@@ -3,183 +3,133 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Util;  //ESTO ES PARA USAR EL EASY FILE
+using Util;  
 
 namespace Videomax
 
-{//Esto estaba en el main, pero lo necesita catalogo para la función findPeliculas
-    public enum OpcionBusqueda { Todas = 1, PorGenero, PorFormato, PorIntervaloDeAños }
-    //Para el compilador, la enumeracion es un entero, cada dato empieza con una posición que es un número
-
+{
+    public enum searchmovie { 
+todo = 1,
+xgenero,
+xformato, 
+xintervaloaños
+}
     class Catalogo
     {
-
-
-
-        //LISTAS
         private List<Formato> formatos;
         private List<Genero> generos;
         private List<Pelicula> peliculas;
         private List<Inventario> inventarios;
 
-        public Catalogo() //Constructor
+        public Catalogo() 
         {
-            //Lo que hace el easyfil es 
-            /* formatos =
-                 EasyFile<Formato>.LoadDataFromFile("formatos.txt", 
-                       tokens =>                          //formo una lista y devueve una lista
-                              new Formato(Convert.ToInt32(tokens[0]), //ASD (esto es una función lamba)
-                 tokens[1],
-                 Convert.ToDecimal(tokens[2])));           //segundo parametro-recibe un string y devuleve un formato
-                 */
-            formatos =
-            EasyFile<Formato>.LoadDataFromFile("formatos.txt", FormatosCallback);
+            
+            formatos = EasyFile<Formato>.LoadDataFromFile("formatos.txt", 
+                tokens => new Formato(Convert.ToInt32(tokens[0]),
+                                                       tokens[1],
+                                   Convert.ToDecimal(tokens[2])));
 
-            generos =
-           EasyFile<Genero>.LoadDataFromFile("generos.txt",
-           tokens =>
-           new Genero(tokens[0], tokens[1]));
+            generos = EasyFile<Genero>.LoadDataFromFile("generos.txt",
+           tokens => new Genero(tokens[0], tokens[1]));
 
-            peliculas =
-          EasyFile<Pelicula>.LoadDataFromFile("peliculas.txt",
-          tokens =>
-          new Pelicula(Convert.ToInt32(tokens[0]),
-                                       tokens[1],
-                                       tokens[2],
-                                       Convert.ToInt32(tokens[3])));
+            peliculas = EasyFile<Pelicula>.LoadDataFromFile("peliculas.txt",
+          tokens => new Pelicula(Convert.ToInt32(tokens[0]),
+                                                  tokens[1],
+                                                  tokens[2],
+                               Convert.ToInt32(tokens[3])));
 
-            inventarios =
-                EasyFile<Inventario>.LoadDataFromFile("inventario.txt",
-                tokens =>
-                new Inventario(Convert.ToInt32(tokens[0]),
-                Convert.ToInt32(tokens[1]),
-                Convert.ToInt32(tokens[2])));
+            inventarios =EasyFile<Inventario>.LoadDataFromFile("inventario.txt",
+                             tokens =>new Inventario(Convert.ToInt32(tokens[0]),
+                                                    Convert.ToInt32(tokens[1]),
+                                                  Convert.ToInt32(tokens[2])));
         }
 
-        //ESTO ES LO MISMO QUE ASD
-        private Formato FormatosCallback(string[] tokens) //Es una función que necesita el easy fil para que convierta la linea de texto
-                                                          //En un formato. Callback es una funcion que se llama por un tercero
-        {
-            return new Formato(Convert.ToInt32(tokens[0]),
-                tokens[1],
-                Convert.ToDecimal(tokens[2]));
-            //El tokens es un arreglo, vemos que en el archivo "formato", existen tres parametros, los cuales necesitamos convertir
-            //a los tipos de datos que necesitan, por eso el convert, 
-        }
-
-
-        public List<PeliculaEnInventario> FindPeliculas(OpcionBusqueda opcion,
-                                                         string generoId = " ",
-                                                         int formatoId = 0,
-                                                         int añoMinimo = -1,
-                                                         int añoMaximo = -1)
+        public List<PeliculaEnInventario> FindPeliculas(searchmovie opcion,
+                                                         string generoId =" ",
+                                                         int formatoId=0,
+                                                         int añoMinimo=-1,
+                                                         int añoMaximo=-1)
         {
             switch (opcion)
             {
-
-                case OpcionBusqueda.PorIntervaloDeAños:
+                case searchmovie.todo:
                     {
-                        List<PeliculaEnInventario> peliculasEnInventario =
-                              new List<PeliculaEnInventario>();
+                        List<PeliculaEnInventario> peliculasEnInventario = new List<PeliculaEnInventario>();
 
-                        añoMinimo = añoMinimo != -1 ? añoMinimo : 0;
-                        añoMaximo = añoMaximo != -1 ? añoMaximo : 3000;
-
-                        foreach (Pelicula p in peliculas.FindAll(p => p.Año >= añoMinimo && p.Año <= añoMaximo))   //listas a Pelicula inventario
+                        foreach (Pelicula p in peliculas)   
                         {
-                            peliculasEnInventario.Add(
-                                new PeliculaEnInventario(
-                                    p.Id, p.GeneroId, p.Titulo, p.Año,
-                                    generos.Find(g => g.Id == p.GeneroId).Descripcion,     //En la lista de genero encuentra un generp qie contenga el ID 
-                                                                                           //Voy a buscar el genero con el idgenero, tengo que mandarle una función que devuelva
-                                                                                           //Un booleano para que me diga cuando coincide el genero con el idgenero
-                                    inventarios.Find(i => i.PeliculaId == p.Id && i.FormatoId == 0).Cantidad, //0 Corresponde a DVD
-                                     inventarios.Find(i => i.PeliculaId == p.Id && i.FormatoId == 1).Cantidad, //1 corresponde a HD
-                                      inventarios.Find(i => i.PeliculaId == p.Id && i.FormatoId == 2).Cantidad//2 corresponde a FULLHD
+                            peliculasEnInventario.Add( new PeliculaEnInventario(p.Id, p.GeneroId, p.Titulo, p.Año,
+                             generos.Find(g => g.Id == p.GeneroId).Descripcion,
+                                    inventarios.Find(i => i.PeliculaId == p.Id && i.FormatoId == 0).Cantidad, 
+                                     inventarios.Find(i => i.PeliculaId == p.Id && i.FormatoId == 1).Cantidad, 
+                                      inventarios.Find(i => i.PeliculaId == p.Id && i.FormatoId == 2).Cantidad
                                     )
                                 );
                         }
-                        return peliculasEnInventario;
-                    }
-
-                case OpcionBusqueda.PorFormato:
-                    {
-                        List<PeliculaEnInventario> peliculasEnInventario =
-                                 new List<PeliculaEnInventario>();
-                        foreach (Inventario i in inventarios.FindAll(i => i.FormatoId == formatoId))
-                        {
-                            Pelicula pelicula = peliculas.Find(p => p.Id == i.PeliculaId);
-                            peliculasEnInventario.Add(
-                              new PeliculaEnInventario(
-                                  pelicula.Id, pelicula.GeneroId, pelicula.Titulo, pelicula.Año,
-                                  generos.Find(g => g.Id == pelicula.GeneroId).Descripcion,     //En la lista de genero encuentra un generp qie contenga el ID 
-                                      formatoId == 0 ? i.Cantidad : 0,
-                                      formatoId == 1 ? i.Cantidad : 0,
-                                      formatoId == 2 ? i.Cantidad : 0)
-
-
-                              );
-                        }
-
 
                         return peliculasEnInventario;
                     }
-
-                case OpcionBusqueda.PorGenero:
+                     case searchmovie.xgenero:
                     {
                         List<PeliculaEnInventario> peliculasEnInventario =
                               new List<PeliculaEnInventario>();
 
                         foreach (Pelicula p in peliculas.FindAll(p => p.GeneroId == generoId))   //listas a Pelicula inventario
                         {
-                            peliculasEnInventario.Add(
-                                new PeliculaEnInventario(
+                            peliculasEnInventario.Add(new PeliculaEnInventario(
                                     p.Id, p.GeneroId, p.Titulo, p.Año,
-                                    generos.Find(g => g.Id == p.GeneroId).Descripcion,     //En la lista de genero encuentra un generp qie contenga el ID 
-                                                                                           //Voy a buscar el genero con el idgenero, tengo que mandarle una función que devuelva
-                                                                                           //Un booleano para que me diga cuando coincide el genero con el idgenero
+                                    generos.Find(g => g.Id == p.GeneroId).Descripcion,     
                                     inventarios.Find(i => i.PeliculaId == p.Id && i.FormatoId == 0).Cantidad, //0 Corresponde a DVD
                                      inventarios.Find(i => i.PeliculaId == p.Id && i.FormatoId == 1).Cantidad, //1 corresponde a HD
-                                      inventarios.Find(i => i.PeliculaId == p.Id && i.FormatoId == 2).Cantidad//2 corresponde a FULLHD
-                                    )
+                                      inventarios.Find(i => i.PeliculaId == p.Id && i.FormatoId == 2).Cantidad)//2 corresponde a FULLHD
                                 );
                         }
                         return peliculasEnInventario;
                     }
-
-                case OpcionBusqueda.Todas:
+                    case searchmovie.xformato:
                     {
                         List<PeliculaEnInventario> peliculasEnInventario =
-                          new List<PeliculaEnInventario>();
-
-                        foreach (Pelicula p in peliculas)   //listas a Pelicula inventario
+                                 new List<PeliculaEnInventario>();
+                        foreach (Inventario i in inventarios.FindAll(i => i.FormatoId == formatoId))
                         {
-                            peliculasEnInventario.Add(
-                                new PeliculaEnInventario(
+                            Pelicula pelicula = peliculas.Find(p => p.Id == i.PeliculaId);
+                            peliculasEnInventario.Add( new PeliculaEnInventario(
+                                  pelicula.Id, pelicula.GeneroId, pelicula.Titulo, pelicula.Año,
+                                  generos.Find(g => g.Id == pelicula.GeneroId).Descripcion,     //En la lista de genero encuentra un generp qie contenga el ID 
+                                      formatoId == 0 ? i.Cantidad : 0,
+                                      formatoId == 1 ? i.Cantidad : 0,
+                                      formatoId == 2 ? i.Cantidad : 0)
+                              );
+                        }
+                        return peliculasEnInventario;
+                    }
+                case searchmovie.xintervaloaños:
+                    {
+                        List<PeliculaEnInventario> peliculasEnInventario = new List<PeliculaEnInventario>();
+
+                        añoMinimo = añoMinimo != -1 ? añoMinimo : 0;
+                        añoMaximo = añoMaximo != -1 ? añoMaximo : 2500;
+
+                        foreach (Pelicula p in peliculas.FindAll(p => p.Año >= añoMinimo && p.Año <= añoMaximo))   
+                        {
+                            peliculasEnInventario.Add(new PeliculaEnInventario(
                                     p.Id, p.GeneroId, p.Titulo, p.Año,
-                                    generos.Find(g => g.Id == p.GeneroId).Descripcion, //En la lista de genero encuentra un generp qie contenga el ID 
-                                                                                       //Voy a buscar el genero con el idgenero, tengo que mandarle una función que devuelva
-                                                                                       //Un booleano para que me diga cuando coincide el genero con el idgenero
-                                    inventarios.Find(i => i.PeliculaId == p.Id && i.FormatoId == 0).Cantidad, //0 Corresponde a DVD
-                                     inventarios.Find(i => i.PeliculaId == p.Id && i.FormatoId == 1).Cantidad, //1 corresponde a HD
-                                      inventarios.Find(i => i.PeliculaId == p.Id && i.FormatoId == 2).Cantidad//2 corresponde a FULLHD
+                                    generos.Find(g => g.Id == p.GeneroId).Descripcion,     
+                                    inventarios.Find(i => i.PeliculaId == p.Id && i.FormatoId == 0).Cantidad, 
+                                     inventarios.Find(i => i.PeliculaId == p.Id && i.FormatoId == 1).Cantidad, 
+                                      inventarios.Find(i => i.PeliculaId == p.Id && i.FormatoId == 2).Cantidad
                                     )
                                 );
                         }
-
                         return peliculasEnInventario;
                     }
                 default:
                     throw new InvalidOperationException("Opcion invalida");
 
             }
-
-
-
         }
         public List<Genero> GetAllGeneros() => new List<Genero>(generos);
 
-        //Copia de la lista
         public List<Formato> GetAllFormatos() => new List<Formato>(formatos);
 
         public bool VerifyPelicula(int peliculaId)
@@ -190,18 +140,6 @@ namespace Videomax
         public Pelicula GetPelicula(int peliculaId)
         {
             return peliculas.Find(p => p.Id == peliculaId);
-        }
-
-        public void AgregarInventario(Pelicula pelicula, int dvd, int blueray, int uhdBlueray)
-        {
-            inventarios.Find(p => pelicula.Id == pelicula.Id && p.FormatoId == 0).Cantidad += dvd;
-
-            inventarios.Find(p => pelicula.Id == pelicula.Id && p.FormatoId == 1).Cantidad += blueray;
-            inventarios.Find(p => pelicula.Id == pelicula.Id && p.FormatoId == 2).Cantidad += uhdBlueray;
-
-            EasyFile<Inventario>.SaveDataToFile("inventario.txt", new string[] { "PeliculaId", "FormatoId", "Cantidad" }, inventarios);
-        }
-
+        } 
     }
 }
-//funcion lamda es una manera de escribir una función lo más corta posible
--- VIDEOMAXX/Program.cs
+++ VIDEOMAXX/Program.cs
@@ -18,7 +18,7 @@ namespace Videomax
         {
             catalogo = new Catalogo();
 
-            int opcion = 0;
+            int opcion = -1;
             do
             {
                 Clear();
@@ -32,12 +32,12 @@ namespace Videomax
                 WriteLine("\t\t\t\t\t0. Salir");
 
                 Write("\n\t\t\t\t\tElige una opción: ");
-                opcion = Convert.ToInt32(Console.ReadLine());
+                opcion = Convert.ToInt32(ReadLine());
 
                 switch (opcion)
                 {
                     case 0:
-                        WriteLine("\n\t\t\t\t\t¡Gracias por utilizar el programa!");
+                        WriteLine("\n\n\t\t\t¡Gracias por utilizar el programa!");
                         break;
 
                     case 1:
@@ -50,7 +50,7 @@ namespace Videomax
                         Ventas();
                         break;
                     default:
-                        WriteLine("\n\t\t\t\t\t¡OPCIÓN NO IMPLEMENTADA!");
+                        WriteLine("\n\t\t\t\t\t¡OPCIÓN NO VÁLIDA!");
 
 
                         break;
@@ -62,12 +62,7 @@ namespace Videomax
 
         }
 
-
-        //Un enumeracion crea un tipo de dato y solo puede tener dichos valores
-
-
-
-        static void BuscarPeliculas() //Es static por el main
+        static void BuscarPeliculas() 
         {
             Clear();
             WriteLine("\t\t\t\t\t-----------------------------------");
@@ -79,81 +74,88 @@ namespace Videomax
             WriteLine("\t\t\t\t\t2. Por género");
             WriteLine("\t\t\t\t\t3. Por formato");
             WriteLine("\t\t\t\t\t4. Por intervalo de años");
+           
 
             Write("\n\t\t\t\t\tElige una opción: ");
-            OpcionBusqueda opcion = (OpcionBusqueda)Convert.ToInt32(Console.ReadLine()); //Hcemos un casting
+            searchmovie opcion = (searchmovie)Convert.ToInt32(Console.ReadLine());
 
             switch (opcion)
             {
-                case OpcionBusqueda.Todas: //Esto es para utilizar los valores de la enumeración
+                case searchmovie.todo:
 
-                    PrintBusquedaPeliculas(catalogo.FindPeliculas(OpcionBusqueda.Todas));
+                    Write("\nLas películas en existencia son las siguientes: \n");
+                    OpcionMostrarTodo(catalogo.FindPeliculas(searchmovie.todo));
                     break;
 
-                case OpcionBusqueda.PorGenero:
+                case searchmovie.xgenero:
 
                     List<Genero> generos = catalogo.GetAllGeneros();
-                    PrintGeneros(generos);
+                    OpcionGenero(generos);
 
+                    Write("\n¿De que género le gustaría?:");
                     int generoIndice = Convert.ToInt32(ReadLine());
 
-                    PrintBusquedaPeliculas(catalogo.FindPeliculas(OpcionBusqueda.PorGenero, generos[generoIndice].Id));
+
+                    OpcionMostrarTodo(catalogo.FindPeliculas(searchmovie.xgenero, generos[generoIndice].Id));
                     break;
 
-                case OpcionBusqueda.PorIntervaloDeAños:
-
+                case searchmovie.xintervaloaños:
+                    INTERVAL: Clear();
                     int añoMinimo = -1;
                     Write("\nDesea especificar un año minimo [s/n]");
                     if (ReadLine().Trim().ToUpper()[0] == 'S')
                     {
+                        Write("\nDigite el año mínimo:\n");
                         añoMinimo = Convert.ToInt32(ReadLine());
                     }
 
                     int añoMaximo = -1;
-                    Write("\nDesea especificar un año minimo [s/n]");
+                    Write("\nDesea especificar un año máximo [s/n]");
                     if (ReadLine().Trim().ToUpper()[0] == 'S')
                     {
+                        Write("\nDigite el año máximo:\n");
                         añoMaximo = Convert.ToInt32(ReadLine());
                     }
+                    if (añoMinimo>añoMaximo){
+                    Write("\n¡DATOS ERRÓNEOS, INTÉNTELO DE NUEVO!");
+                        ReadKey();
+                    goto INTERVAL;
+                        }
 
-
-
-                    PrintBusquedaPeliculas(catalogo.FindPeliculas(OpcionBusqueda.PorIntervaloDeAños, añoMinimo: añoMinimo, añoMaximo: añoMaximo));
+                    OpcionMostrarTodo(catalogo.FindPeliculas(searchmovie.xintervaloaños, añoMinimo: añoMinimo, añoMaximo: añoMaximo));
+                    ReadKey();
+                   
                     break;
 
-                case OpcionBusqueda.PorFormato:
+                case searchmovie.xformato:
                     List<Formato> formatos = catalogo.GetAllFormatos();
-                    PrintFormatos(formatos);
+                    OpcionFormato(formatos);
 
+                     Write("\n¿Qué tipo de formato desea?\n");
                     int formatoIndice = Convert.ToInt32(ReadLine());
+                   
 
-
-                    //PrintBusquedaPeliculas(catalogo.FindPeliculas(OpcionBusqueda.PorFormato,"" ,formatos[formatoIndice].Id));
-                    PrintBusquedaPeliculas(catalogo.FindPeliculas(OpcionBusqueda.PorFormato, formatoId: formatos[formatoIndice].Id));//OTRA FORMA-POR NOMBRE
+                    OpcionMostrarTodo(catalogo.FindPeliculas(searchmovie.xformato, formatoId: formatos[formatoIndice].Id));
                     break;
 
                 default:
-                    WriteLine("\n¡OPCIÓN NO IMPLEMENTADA!");
-
+                    WriteLine("\n¡OPCIÓN NO VÁLIDA!");
                     break;
 
             }
-
+            
         }
 
-        static void PrintBusquedaPeliculas(List<PeliculaEnInventario> peliculas) //Dentro de <> va el tipo de dato de la lista
+        static void OpcionMostrarTodo(List<PeliculaEnInventario> peliculas) 
         {
             WriteLine("\n**** RESULTADO DE BÚSQUEDA ****");
 
-            if (peliculas.Count > 0) //Count es el numero de elementosque hay en la lista
+            if (peliculas.Count > 0) 
             {
-                foreach (PeliculaEnInventario p in peliculas)
+                foreach (var p in peliculas)
                 {
-                    WriteLine($"{p.Id} - {p.Titulo} - {p.Genero}.({p.Año}).   [VD:{p.DVD}, BR:{p.BlueRay}, UHDBR:{p.UHDBlueRay}].");
+                    WriteLine($"{p.Id} - {p.Titulo} - {p.Genero}.({p.Año}).   [DVD:{p.DVD}, BR:{p.BlueRay}, UHDBR:{p.UHDBlueRay}].");
                 }
-
-
-
 
             }
             else
@@ -165,19 +167,19 @@ namespace Videomax
 
 
         }
-        static void PrintGeneros(List<Genero> generos)
+        static void OpcionGenero(List<Genero> generos)
         {
 
             Clear();
             WriteLine("\n**** GENEROS ****");
 
-            for (int i = 0; i < generos.Count; ++i)
-            {
-                WriteLine($"{i}. {generos[i].Descripcion}");
-            }
+                for (int i=0; i< generos.Count; ++i)
+{
+                WriteLine($"{i} --- {generos[i].Descripcion}");
+                    }
         }
 
-        static void PrintFormatos(List<Formato> formatos)
+        static void OpcionFormato(List<Formato> formatos)
         {
 
             Clear();
@@ -185,10 +187,8 @@ namespace Videomax
 
             for (int i = 0; i < formatos.Count; ++i)
             {
-                WriteLine($"{i}. {formatos[i].Descripcion}");
+                WriteLine($"{i} {formatos[i].Descripcion}");
             }
-
-
 
         }
         static void SubMenuInventario()
@@ -209,7 +209,7 @@ namespace Videomax
                 WriteLine("\t\t\t\t\t0. Salir");
 
                 Write("\nElige una opción: ");
-                opcion = Convert.ToInt32(Console.ReadLine());
+                opcion = Convert.ToInt32(ReadLine());
 
                 switch (opcion)
                 {
